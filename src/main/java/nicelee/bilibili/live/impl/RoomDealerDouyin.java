889bac73c83d4ab774a110a7c42eeb2a20b73955
lijia <lijia0732@sina.com>
2021-08-30 15:55:56 +0800

    增加douyin清晰度选择

--- src/main/java/nicelee/bilibili/live/impl/RoomDealerDouyin.java
+++ src/main/java/nicelee/bilibili/live/impl/RoomDealerDouyin.java
@@ -12,7 +12,7 @@ import org.json.JSONObject;
 
 import nicelee.bilibili.live.RoomDealer;
 import nicelee.bilibili.live.domain.RoomInfo;
-import nicelee.bilibili.util.HttpRequestUtil;
+import nicelee.bilibili.util.Logger;
 
 public class RoomDealerDouyin extends RoomDealer {
 
@@ -32,36 +32,45 @@ public class RoomDealerDouyin extends RoomDealer {
 	 */
 	@Override
 	public RoomInfo getRoomInfo(String shortId) {
-		if(shortId.startsWith("https://v.douyin.com")) {
+		if (shortId.startsWith("https://v.douyin.com")) {
 			try {
 				URL url = new URL(shortId);
 				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 				conn.setInstanceFollowRedirects(false);
+				conn.setRequestProperty("User-Agent",
+						"Mozilla/5.0 (Android 9.0; Mobile; rv:68.0) Gecko/68.0 Firefox/68.0");
 				conn.connect();
-				
 				String location = conn.getHeaderField("Location");
-				System.out.println(location);
-				if(location != null) {
+				Logger.println(location);
+				if (location != null && location.startsWith("https://www.iesdouyin.com")) {
+					url = new URL(location);
+					conn = (HttpURLConnection) url.openConnection();
+					conn.setInstanceFollowRedirects(false);
+					conn.setRequestProperty("User-Agent",
+							"Mozilla/5.0 (Android 9.0; Mobile; rv:68.0) Gecko/68.0 Firefox/68.0");
+					conn.connect();
+					location = conn.getHeaderField("Location");
+					Logger.println(location);
+				}
+
+				if (location != null && location.startsWith("https://webcast.amemv.com")) {
 					Matcher matcher = pShortId.matcher(location);
-					if(matcher.find())
+					if (matcher.find())
 						shortId = matcher.group(1);
 				}
 			} catch (IOException e) {
 			}
 		}
-		
-		
+
 		RoomInfo roomInfo = new RoomInfo();
 		roomInfo.setShortId(shortId);
 		try {
 			String roomId = shortId;
 
-			HttpRequestUtil util = new HttpRequestUtil();
 			HashMap<String, String> mobile = new HashMap<>();
 			mobile.put("User-Agent", "Mozilla/5.0 (Android 9.0; Mobile; rv:68.0) Gecko/68.0 Firefox/68.0");
 			mobile.put("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
 			mobile.put("Accept-Language", "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2");
-			mobile.put("Accept-Encoding", "gzip");
 			String html = util.getContent("https://webcast.amemv.com/webcast/reflow/" + roomId, mobile);
 
 			Matcher matcher = pJson.matcher(html);
@@ -70,25 +79,26 @@ public class RoomDealerDouyin extends RoomDealer {
 					.getJSONObject("room");
 			JSONObject owner = json.getJSONObject("owner");
 			JSONObject stream_url = json.getJSONObject("stream_url");
-			
-			
+
 			roomInfo.setUserName(owner.getString("nickname"));
 			roomInfo.setRoomId(roomId);
 			roomInfo.setUserId(owner.optLong("short_id"));
-			if(html.contains("直播已结束")) {
+			if (html.contains("直播已结束")) {
 				roomInfo.setLiveStatus(0);
-			}else {
+			} else {
 				roomInfo.setLiveStatus(1);
 				roomInfo.setTitle(json.getString("title"));
 				roomInfo.setDescription(owner.getString("signature"));
-				JSONArray jArray = stream_url.getJSONObject("live_core_sdk_data").getJSONObject("pull_data").getJSONObject("options").getJSONArray("qualities");
-				
-				String[] qn = new String[jArray.length()];
-				String[] qnDesc = new String[jArray.length()];
-				for (int i = 0; i < jArray.length(); i++) {
+				JSONArray jArray = stream_url.getJSONObject("live_core_sdk_data").getJSONObject("pull_data")
+						.getJSONObject("options").getJSONArray("qualities");
+				int qualityLen = jArray.length();
+				String[] qn = new String[qualityLen];
+				String[] qnDesc = new String[qualityLen];
+				for (int i = 0; i < qualityLen; i++) {
 					JSONObject obj = jArray.getJSONObject(i);
-					qn[i] = "" + obj.optInt("level");
-					qnDesc[i] = obj.getString("name");
+					int level = obj.optInt("level");
+					qn[i] = "" + i;
+					qnDesc[qualityLen - level] = obj.getString("name");
 				}
 				roomInfo.setAcceptQuality(qn);
 				roomInfo.setAcceptQualityDesc(qnDesc);
@@ -104,12 +114,10 @@ public class RoomDealerDouyin extends RoomDealer {
 	@Override
 	public String getLiveUrl(String roomId, String qn, Object... obj) {
 		try {
-			HttpRequestUtil util = new HttpRequestUtil();
 			HashMap<String, String> mobile = new HashMap<>();
 			mobile.put("User-Agent", "Mozilla/5.0 (Android 9.0; Mobile; rv:68.0) Gecko/68.0 Firefox/68.0");
 			mobile.put("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
 			mobile.put("Accept-Language", "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2");
-			mobile.put("Accept-Encoding", "gzip");
 			String html = util.getContent("https://webcast.amemv.com/webcast/reflow/" + roomId, mobile);
 
 			Matcher matcher = pJson.matcher(html);
@@ -118,8 +126,26 @@ public class RoomDealerDouyin extends RoomDealer {
 					.getJSONObject("room");
 			JSONObject stream_url = json.getJSONObject("stream_url");
 
-			System.out.printf("查询%s的链接，得到默认链接\r\n", qn);
-			return stream_url.getString("rtmp_pull_url");
+			JSONArray flv_sources = stream_url.getJSONObject("live_core_sdk_data").getJSONObject("pull_data")
+					.getJSONObject("options").getJSONArray("qualities");
+			int flv_sources_len = flv_sources.length();
+			String sdk_key = null;
+			for (int i = 0; i < flv_sources_len; i++) {
+				JSONObject quality = flv_sources.getJSONObject(i);
+				int level = quality.getInt("level");
+				// 这里要与前面一致
+				if (qn.equals("" + (flv_sources_len - level))) {
+					sdk_key = quality.getString("sdk_key");
+					break;
+				}
+			}
+
+			String pull_data = stream_url.getJSONObject("live_core_sdk_data").getJSONObject("pull_data")
+					.getString("stream_data");
+			JSONObject data = new JSONObject(pull_data).getJSONObject("data");
+			String link = data.getJSONObject(sdk_key).getJSONObject("main").getString("flv");
+			Logger.println(link);
+			return link;
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
