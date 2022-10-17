9d7fd1f96fcf3bd9f5bf667814e3184ccfd29838
lijia <lijia0732@sina.com>
2022-10-17 12:09:41 +0800

    update v2.22.0

--- src/main/java/nicelee/bilibili/Main.java
+++ src/main/java/nicelee/bilibili/Main.java
@@ -14,7 +14,7 @@ import nicelee.bilibili.threads.ThRecord;
 
 public class Main {
 
-	final static String version = "v2.21.0";
+	final static String version = "v2.22.0";
 	public static Thread thRecord;
 	public static Thread thMonitor;
 	public static Thread thCommand;
